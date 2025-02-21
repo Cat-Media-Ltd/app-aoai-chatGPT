2ac3d1067fa488731ed6d57d06f074b7052b30a1
juanzabala06 <58573100+juanzabala06@users.noreply.github.com>
2025-02-21 16:49:17 +0000

    Manifest updated

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,23 +2,41 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/static/', // tells Vite assets are served from /static/
   plugins: [
     react(),
-    VitePWA({ registerType: 'autoUpdate', 
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      registerType: 'autoUpdate',
       manifest: {
-        theme_color: '#ffffff', // Add your desired theme color here
-      }
-
-    })
+        name: 'HubSpot Expert by Cat Media',
+        short_name: 'HubSpot Expert',
+        description: 'HubSpot Expert by Cat Media',
+        theme_color: '#f3f3f3',
+        
+        icons: [
+          {
+            src: 'Cat Media Mascot.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'Cat Media Mascot.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
   ],
   build: {
     outDir: '../static',
     emptyOutDir: true,
-    sourcemap: true
+    sourcemap: true,
+    manifest: true,
   },
+
   server: {
     proxy: {
       '/ask': 'http://localhost:5000',
