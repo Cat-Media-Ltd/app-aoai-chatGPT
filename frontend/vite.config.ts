d0a73a4cb8aea433e8a2973985598ca7c69d7552
juanzabala06 <58573100+juanzabala06@users.noreply.github.com>
2025-02-21 16:37:17 +0000

    manifest

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,19 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({ registerType: 'autoUpdate', 
+      manifest: {
+        theme_color: '#ffffff', // Add your desired theme color here
+      }
+
+    })
+  ],
   build: {
     outDir: '../static',
     emptyOutDir: true,
